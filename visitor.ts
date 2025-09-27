8c1f99f018e28802bb7458932e15e98224457195
Balin Sinnott <sinnott.balin@gmail.com>
2025-09-26 18:11:26 -0600

    use auth handler from importing application

--- visitor.ts
+++ visitor.ts
@@ -418,6 +418,9 @@ export class ReactApolloVisitor extends ClientSideBaseVisitor<
     this.imports.add(this.getApolloReactCommonImport(true));
     this.imports.add(this.getApolloReactHooksImport(false));
     this.imports.add("import { getClient } from '@/lib/apolloClient';");
+    this.imports.add(
+      "import { withAuthHandler } from '@/lib/withAuthHandler';"
+    );
     this.imports.add(this.getDefaultOptions());
 
     const typeToHook = (type: string): string => {
@@ -521,10 +524,12 @@ export class ReactApolloVisitor extends ClientSideBaseVisitor<
         `export function ${serverOperationName}(baseOptions${
           hasRequiredVariables ? "" : "?"
         }: ${this.getApolloReactHooksIdentifier()}.${operationType}HookOptions<${operationResultType}, ${operationVariablesTypes}>) {
-          return getClient().query<${operationResultType}, ${operationVariablesTypes}>({ query: ${this.getDocumentNodeVariable(
+          return withAuthHandler(
+            getClient().query<${operationResultType}, ${operationVariablesTypes}>({ query: ${this.getDocumentNodeVariable(
           node,
           documentVariableName
-        )}, variables: baseOptions?.variables});
+        )}, variables: baseOptions?.variables})
+          );
         }`
       );
       hookResults.push(
